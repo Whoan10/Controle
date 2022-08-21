cc66dd5d50fafc36eeeda6956ca97cef272094ce
Whoan10 <juanvitorioclash@gmail.com>
2022-08-21 19:20:08 -0300

    sei la

--- controle_gastos/settings.py
+++ controle_gastos/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import django_heroku
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f7*6_dvh%39koo3p4yl(s%mop4!3e*dtzagi+ood8_2-%8v(92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://projetojuanzin.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
